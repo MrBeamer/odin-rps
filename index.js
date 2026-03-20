ffbdfbf635874d71264f618fca83c6b9d9d94a3d
Michael Beamer <michael@Air-von-Michael.home>
2026-03-20 13:20:07 +0100

    Finish refactor of code

--- index.js
+++ index.js
@@ -1,94 +1,177 @@
-//Keep count of the Score
-let playerScore = 0;
-let computerScore = 0;
-let gameRound = 1;
-let isGameOver = false;
-const roundNumber = document.querySelector(".round-number");
-const roundResultText = document.querySelector(".game-result");
-const scoreComputer = document.querySelector(".score-computer");
-const scorePlayer = document.querySelector(".score-player");
-const buttons = document.querySelector(".choices-container");
-const resetButton = document.querySelector("#restart");
-
-//Helper function generates random number between 0 and 2
-const getRandomNumber = () => Math.floor(Math.random() * 3);
-
-// Returns a random computer choice by using a random number as the array index
-const getComputerChoice = () => {
-  const options = ["rock", "paper", "scissors"];
-  return options[getRandomNumber()];
-};
-
-const calculateRoundResult = (playersChoice, computerChoice) => {
-  if (playersChoice === computerChoice) {
-    return `Computer: ${computerChoice} | It is a draw.`;
-  } else if (
-    (playersChoice === "rock" && computerChoice === "scissors") ||
-    (playersChoice === "paper" && computerChoice === "rock") ||
-    (playersChoice === "scissors" && computerChoice === "paper")
-  ) {
-    playerScore++;
-    return `Computer: ${computerChoice} | Player wins.`;
-  } else {
-    computerScore++;
-    return `Computer: ${computerChoice} | Computer wins.`;
+class GameModel {
+  // Holds game Data, possible to set and get data from outside
+  _gameRound = 1;
+  _isGameOver = false;
+  constructor() {
+    this._playerScore = 0;
+    this._computerScore = 0;
   }
-};
 
-// Prints rounds result
-const printMessage = (roundResult) => {
-  roundResultText.textContent = roundResult;
-  scorePlayer.textContent = playerScore;
-  scoreComputer.textContent = computerScore;
-};
-
-// Starts a round of the game
-const playRound = (userInput) => {
-  const computerChoice = getComputerChoice();
-  const roundResult = calculateRoundResult(userInput, computerChoice);
-  printMessage(roundResult);
-  gameRound++;
-  if (gameRound <= 5) {
-    roundNumber.textContent = gameRound;
+  incrementPlayerScore() {
+    ++this._playerScore;
   }
-};
 
-const declareWinner = () => {
-  if (playerScore > computerScore) {
-    roundResultText.textContent = "GAME OVER: You beat the machine!";
-  } else if (playerScore < computerScore) {
-    roundResultText.textContent = "GAME OVER: The computer wins this time.";
-  } else {
-    roundResultText.textContent = "GAME OVER: It's a total tie!";
+  get playerScore() {
+    return this._playerScore;
   }
-};
 
-const playGame = (userInput) => {
-  console.log(userInput);
-
-  if (gameRound <= 5) {
-    playRound(userInput);
+  set playerScore(number) {
+    this._playerScore = number;
   }
-  if (gameRound > 5) {
-    declareWinner();
-    buttons.removeEventListener("click", getPlayerChoice);
+
+  incrementComputerScore() {
+    ++this._computerScore;
   }
-};
 
-const resetGame = () => {
-  playerScore = 0;
-  computerScore = 0;
-  gameRound = 1;
-  roundNumber.textContent = gameRound;
-  scorePlayer.textContent = playerScore;
-  scoreComputer.textContent = computerScore;
-  roundResultText.textContent = "Make your choice";
-  buttons.addEventListener("click", getPlayerChoice);
-};
+  get computerScore() {
+    return this._computerScore;
+  }
 
-const getPlayerChoice = (event) => {
-  const playerChoice = event.target.id;
-  playGame(playerChoice);
-};
-buttons.addEventListener("click", getPlayerChoice);
-resetButton.addEventListener("click", resetGame);
+  set computerScore(number) {
+    this._computerScore = number;
+  }
+
+  set upDateGameOver(state) {
+    this._isGameOver = state;
+  }
+
+  set gameRound(str) {
+    this._gameRound = str;
+  }
+
+  get gameRound() {
+    return this._gameRound;
+  }
+
+  incrementGameRound() {
+    ++this._gameRound;
+  }
+
+  resetModel() {
+    this.computerScore = 0;
+    this.playerScore = 0;
+    this.gameRound = 1;
+  }
+}
+//////////////////////////////////////////////////////////////////////////////
+class GameView {
+  // Make all required dom elements available
+  _roundNumber = document.querySelector(".round-number");
+  _roundResultText = document.querySelector(".game-result");
+  _scoreComputer = document.querySelector(".score-computer");
+  _scorePlayer = document.querySelector(".score-player");
+  _buttons = document.querySelector(".choices-container");
+  _resetButton = document.querySelector("#restart");
+
+  // UI displays & updates Scores and Round
+  printNumber(roundNumber, playerScore, computerScore) {
+    this._roundNumber.textContent = roundNumber;
+    this._scorePlayer.textContent = playerScore;
+    this._scoreComputer.textContent = computerScore;
+  }
+  // UI displays winner or loser of the round
+  printRoundResultText(text) {
+    this._roundResultText.textContent = text;
+  }
+
+  resetView() {
+    this.printNumber(0, 0, 0);
+    this.printRoundResultText("Make your choice");
+  }
+}
+//////////////////////////////////////////////////////////////////////////////
+class GameController {
+  constructor() {
+    this._view = new GameView();
+    this._model = new GameModel();
+    this._view._buttons.addEventListener("click", (event) =>
+      this.getPlayerChoice(event),
+    );
+    this._view._resetButton.addEventListener("click", (event) =>
+      this.resetGame(event),
+    );
+  }
+
+  //Helper function generates random number between 0 and 2
+  getRandomNumber() {
+    return Math.floor(Math.random() * 3);
+  }
+
+  // Returns a random computer choice by using a random number as the array index
+  getComputerChoice() {
+    const options = ["rock", "paper", "scissors"];
+    return options[this.getRandomNumber()];
+  }
+
+  // Calculate winner based on player input and computer random input
+  calculateRoundResult(playersChoice, computerChoice) {
+    if (playersChoice === computerChoice) {
+      return `Computer: ${computerChoice} | It is a draw.`;
+    } else if (
+      (playersChoice === "rock" && computerChoice === "scissors") ||
+      (playersChoice === "paper" && computerChoice === "rock") ||
+      (playersChoice === "scissors" && computerChoice === "paper")
+    ) {
+      this._model.incrementPlayerScore();
+      return `Computer: ${computerChoice} | Player wins.`;
+    } else {
+      this._model.incrementComputerScore();
+      return `Computer: ${computerChoice} | Computer wins.`;
+    }
+  }
+
+  // Starts a round of the game
+  playRound(userInput) {
+    const computerChoice = this.getComputerChoice();
+    const roundResult = this.calculateRoundResult(userInput, computerChoice);
+    const playerScore = this._model.playerScore;
+    const computerScore = this._model.computerScore;
+    console.log(roundResult);
+    this._model.incrementGameRound();
+    const gameRound = this._model.gameRound;
+    this._view.printNumber(gameRound, playerScore, computerScore);
+    this._view.printRoundResultText(roundResult);
+  }
+
+  // Creates the game frame how many rounds played and when winner is declared
+  playGame(userInput) {
+    const gameRound = this._model.gameRound;
+
+    if (gameRound < 5) {
+      this.playRound(userInput);
+    }
+    if (gameRound === 5) {
+      this.declareWinner();
+      return;
+    }
+  }
+
+  // Get player input
+  getPlayerChoice(event) {
+    const playerChoice = event.target.id;
+    this.playGame(playerChoice);
+  }
+
+  // Based on the end player and computer score winner gets declared
+  declareWinner() {
+    const playerScore = this._model.playerScore;
+    const computerScore = this._model.computerScore;
+    if (playerScore > computerScore) {
+      this._view.printRoundResultText("GAME OVER: You beat the machine!");
+    } else if (playerScore < computerScore) {
+      this._view.printRoundResultText(
+        "GAME OVER: The computer wins this time.",
+      );
+    } else {
+      this._view.printRoundResultText("GAME OVER: It's a total tie!");
+    }
+  }
+
+  // Resets the UI and the game data
+  resetGame() {
+    this._model.resetModel();
+    this._view.resetView();
+  }
+}
+// Initiates the Game (without this controller is not usable)
+const game = new GameController();
